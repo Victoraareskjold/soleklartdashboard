9f1f9d5c4666d5856fed1569efa463980e059e68
Victor Aareskjold <victor.aareskjold@icloud.com>
2025-12-04 17:28:17 +0100

    first

--- app/(dashboard)/contacts/page.tsx
+++ app/(dashboard)/contacts/page.tsx
@@ -19,14 +19,27 @@ export type ContactLead = {
   assigned_to: string | null;
 };
 
+export type CreateLead = {
+  email: string | null;
+  person_info: string | null;
+  address: string | null;
+  mobile: string | null;
+  phone: string | null;
+  assigned_to: string | null;
+};
+
 export default function ContactsPage() {
   const { teamId } = useTeam();
   const { installerGroupId } = useInstallerGroup();
   const { user } = useAuth();
 
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   const [team, setTeam] = useState<Team>();
   const [selectedMember, setSelectedMember] = useState<string>("");
   const [coldCalls, setColdCalls] = useState<ContactLead[]>([]);
+
+  const [formData, setFormData] = useState<CreateLead>({} as CreateLead);
 
   useEffect(() => {
     if (!teamId) return;
@@ -45,7 +58,7 @@ export default function ContactsPage() {
         installerGroupId,
         teamId,
         // Status 6 betyr contact, har ingen annen referanse
-        status: "5",
+        status: "6",
       });
 
       const res = await fetch(`/api/coldCalling?${params.toString()}`, {
@@ -80,13 +93,25 @@ export default function ContactsPage() {
     "phone",
   ];
 
+  const formDataFields: {
+    label: string;
+    value: keyof CreateLead;
+    type: "text" | "select" | "number";
+  }[] = [
+    { label: "E-post", value: "email", type: "text" },
+    { label: "Navn", value: "person_info", type: "text" },
+    { label: "Mobil", value: "mobile", type: "text" },
+    { label: "Telefon", value: "phone", type: "text" },
+    { label: "Adresse", value: "address", type: "text" },
+  ];
+
   if (!user || !team) return <LoadingScreen />;
 
   const assignedTo = team?.members?.find(
     (member) => member.user_id === user.id
   );
 
-  const handleCreateLead = async (id: string) => {
+  const handleUpdateToLead = async (id: string) => {
     try {
       const res = await fetch("/api/coldCalling/contact/upsert", {
         method: "PATCH",
@@ -103,66 +128,145 @@ export default function ContactsPage() {
     }
   };
 
-  return (
-    <div>
-      <h1>Kontakter</h1>
-      <div className="flex flex-row justify-between mb-4">
-        <div className="flex flex-col gap-2">
-          <TeamMemberSelector
-            team={team}
-            selectedMember={selectedMember}
-            onSelectMember={setSelectedMember}
-            defaultUser={user.id}
-          />
-          <input
-            type="text"
-            placeholder="Søk etter navn eller beskrivelse"
-            className="border p-2 rounded-md"
-          />
-        </div>
-        <div>
-          <button className="w-42 text-center rounded-sm px-4 py-2 bg-[#FF8E4C] text-white">
-            Opprett konktakt
-          </button>
-        </div>
-      </div>
+  const handleCreateLead = async () => {
+    try {
+      const res = await fetch("/api/coldCalling/contact/upsert", {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
 
-      <table className="w-full">
-        <thead>
-          <tr>
-            <th className="w-1/10 text-left"></th>
-            {headers.map((header, index) => (
-              <th className="border p-2 w-1/8 text-left" key={index}>
-                {header}
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {coldCalls.map((coldCall) => (
-            <tr key={coldCall.id}>
-              <td className="w-1/10 border">
-                <button
-                  onClick={() => handleCreateLead(coldCall.id)}
-                  className="w-full bg-[#FF8E4C] h-14 text-white"
-                >
-                  Opprett avtale
-                </button>
-              </td>
-              {fields.map((field) => (
-                <td className="border p-1 w-1/8" key={field}>
-                  {coldCall[field]}
-                </td>
+      if (!res.ok) throw new Error("Feil ved oppretelse av avtale");
+
+      toast.success("Avtale opprettet!");
+    } catch (err) {
+      console.error(err);
+      toast.error("Noe gikk galt");
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <h1>Kontakter</h1>
+        <div className="flex flex-row justify-between mb-4">
+          <div className="flex flex-col gap-2">
+            <TeamMemberSelector
+              team={team}
+              selectedMember={selectedMember}
+              onSelectMember={setSelectedMember}
+              defaultUser={user.id}
+            />
+            <input
+              type="text"
+              placeholder="Søk etter navn eller beskrivelse"
+              className="border p-2 rounded-md"
+            />
+          </div>
+          <div>
+            <button
+              onClick={() => setIsModalOpen(true)}
+              className="w-42 text-center rounded-sm px-4 py-2 bg-[#FF8E4C] text-white"
+            >
+              Opprett konktakt
+            </button>
+          </div>
+        </div>
+
+        <table className="w-full">
+          <thead>
+            <tr>
+              <th className="w-1/10 text-left"></th>
+              {headers.map((header, index) => (
+                <th className="border p-2 w-1/8 text-left" key={index}>
+                  {header}
+                </th>
               ))}
-              <td className="border p-1 w-1/8">
-                {assignedTo?.name ||
-                  assignedTo?.user_id ||
-                  "Ingen leadinnhenter"}
-              </td>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody>
+            {coldCalls.map((coldCall) => (
+              <tr key={coldCall.id}>
+                <td className="w-1/10 border">
+                  <button
+                    onClick={() => handleUpdateToLead(coldCall.id)}
+                    className="w-full bg-[#FF8E4C] h-14 text-white"
+                  >
+                    Opprett avtale
+                  </button>
+                </td>
+                {fields.map((field) => (
+                  <td className="border p-1 w-1/8" key={field}>
+                    {coldCall[field]}
+                  </td>
+                ))}
+                <td className="border p-1 w-1/8">
+                  {assignedTo?.name ||
+                    assignedTo?.user_id ||
+                    "Ingen leadinnhenter"}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      {isModalOpen && (
+        <div className="absolute top-0 right-0 w-156 bg-blue-50 h-full shadow-lg z-10">
+          <div className="bg-[#85F2AD] p-4">
+            <h1 className="text-2xl font-semibold text-white">
+              Opprett kontakt
+            </h1>
+          </div>
+          <div className="p-4">
+            <div className="grid grid-cols-2 gap-2">
+              {formDataFields.map((field) => (
+                <div key={field.value} className="flex flex-col gap-1">
+                  <label>{field.label}</label>
+                  {field.type === "text" && (
+                    <input
+                      className="border p-1.5 rounded-md"
+                      type="text"
+                      value={formData[field.value] ?? ""}
+                      onChange={(e) =>
+                        setFormData((prev) => ({
+                          ...prev,
+                          [field.value]: e.target.value,
+                        }))
+                      }
+                    />
+                  )}
+                  {field.type === "number" && (
+                    <input
+                      className="border p-1.5 rounded-md"
+                      type="number"
+                      value={formData[field.value] ?? ""}
+                      onChange={(e) =>
+                        setFormData((prev) => ({
+                          ...prev,
+                          [field.value]: e.target.value,
+                        }))
+                      }
+                    />
+                  )}
+                </div>
+              ))}
+              <div className="flex flex-col gap-1">
+                <label>Lead-innhenter</label>
+                <TeamMemberSelector
+                  team={team}
+                  selectedMember={selectedMember}
+                  onSelectMember={setSelectedMember}
+                  defaultUser={user.id}
+                />
+              </div>
+            </div>
+            <div>
+              <button onClick={handleCreateLead}>Opprett</button>
+              <button onClick={() => setIsModalOpen(false)}>Avbryt</button>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
--- app/api/coldCalling/contact/upsert/route.ts
+++ app/api/coldCalling/contact/upsert/route.ts
@@ -1,6 +1,40 @@
 import { createSupabaseAdminClient } from "@/utils/supabase/client";
 import { NextRequest, NextResponse } from "next/server";
 
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+    const id = body;
+
+    if (!id) {
+      return NextResponse.json({ error: "Mangler id" }, { status: 400 });
+    }
+
+    const supabase = createSupabaseAdminClient();
+
+    const { error } = await supabase
+      .from("leads")
+      .insert({
+        status: 7,
+        updated_at: new Date().toISOString(),
+      })
+      .eq("id", id)
+      .select();
+
+    if (error) {
+      return NextResponse.json(
+        { error: `Feil ved opprettelse av avtale ${id}` },
+        { status: 500 }
+      );
+    }
+
+    return NextResponse.json({ success: true });
+  } catch (error) {
+    console.error("Import error:", error);
+    return NextResponse.json({ error: "Server error" }, { status: 500 });
+  }
+}
+
 export async function PATCH(req: NextRequest) {
   try {
     const body = await req.json();
